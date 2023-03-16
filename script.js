3302a4796f34e76795eefc6c2d4d4376ccaee726
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-03-16 14:09:57 +0800

    Add color choices and reset buttons

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
+const container = document.querySelector('.container');
+container.setAttribute('style', 'display: flex; align-items: center; flex-direction: column;')
+let color = 'black';
+
 function makeGrid(size){
     const board = document.querySelector('.board');
     board.setAttribute('style',
-                        `display: grid; height: 500px; width: 500px; grid-template-columns: repeat(${size},1fr); grid-template-rows: repeat(${size},1fr)`);
-
+                        `display: grid; height: 250px; width: 250px; grid-template-columns: repeat(${size},1fr); grid-template-rows: repeat(${size},1fr)`);
     amount = size * size;
     for (let i = 0;i < amount;i++){
         let squares = document.createElement('div');
-        squares.setAttribute('style','background-color: black');
-        squares.addEventListener('mouseenter', () => squares.setAttribute('style',
-                                                                            'background-color: white'));
-        squares.addEventListener('mouseleave', () => squares.setAttribute('style',
-                                                                            'background-color: white'));                                                                    
+        squares.addEventListener('mouseenter', colorSquare);
+        squares.addEventListener('mouseleave', colorSquare);
+        squares.setAttribute('style','background-color: white');                                                                   
         board.insertAdjacentElement("beforeend", squares);
     }
 }
 
 function inputGridSize(amount){
-    if (amount >= 1  || amount <= 100){
+    if (amount >= 1  || amount <= 30){
         makeGrid(amount);
     } else {
         alert('Invalid Amount of Squares');
     }
 }
 
-makeGrid(16);
+const input = document.querySelector('.inputSlider');
 
-const inputs = document.querySelector('.inputSlider');
+function handleUpdate(){
+    inputGridSize(this.value);
+}
 
-inputs.forEach(input => input.addEventListener('change'));
-inputs.forEach(input => input.addEventListener('mousemove'));
+input.addEventListener('change', handleUpdate);
+input.addEventListener('mousemove', handleUpdate);
+
+function colorSquare(){
+    if (color === 'random'){
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.style.backgroundColor = 'white');
+}

80cdd200c99a7ea8f0248472d5514f11391711f5
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-03-16 11:57:37 +0800

    Add hover effect to div grid

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const board = document.querySelector('.board');
-board.setAttribute('style',
-                    'display: grid; height: 500px; width: 500px; grid-template-columns: repeat(16,1fr); grid-template-rows: repeat(16,1fr)');
-for (let i = 0;i < 256;i++){
-    let squares = document.createElement('div');
-    squares.setAttribute('style','background-color: black');
-    squares
-    board.insertAdjacentElement("beforeend", squares);
+function makeGrid(size){
+    const board = document.querySelector('.board');
+    board.setAttribute('style',
+                        `display: grid; height: 500px; width: 500px; grid-template-columns: repeat(${size},1fr); grid-template-rows: repeat(${size},1fr)`);
+
+    amount = size * size;
+    for (let i = 0;i < amount;i++){
+        let squares = document.createElement('div');
+        squares.setAttribute('style','background-color: black');
+        squares.addEventListener('mouseenter', () => squares.setAttribute('style',
+                                                                            'background-color: white'));
+        squares.addEventListener('mouseleave', () => squares.setAttribute('style',
+                                                                            'background-color: white'));                                                                    
+        board.insertAdjacentElement("beforeend", squares);
+    }
 }
+
+
+
